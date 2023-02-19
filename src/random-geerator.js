5e0f96f9316636e4f2a1a54124237c7627985a57
maxim2310 <m23a09x96@gmail.com>
2023-02-19 16:52:54 +0200

    added new generator

--- src/random-geerator.js
+++ src/random-geerator.js
@@ -1,7 +1,23 @@
 'use strict';
 
 function generateRandomFourDigitNumber() {
-  return Math.floor(1000 + Math.random() * 9000);
+  const digits = [...Array(10).keys()]; // create an array of digits from 1 to 9
+  let number = '';
+
+  while (number.length < 4) {
+    const index = Math.floor(Math.random() * digits.length);
+    const digit = digits[index];
+
+    digits.splice(index, 1);
+
+    if (number.length === 0 && digit === '0') {
+      continue;
+    }
+
+    number += digit;
+  }
+
+  return number;
 }
 
 module.exports = { generateRandomFourDigitNumber };
